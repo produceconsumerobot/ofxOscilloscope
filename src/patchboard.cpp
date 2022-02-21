78ea1bfb4b795c4fa6fc6feb7972bc9c7a4e90d1
Nitin Nair <nitin7nair@gmail.com>
2022-02-21 12:26:46 -0500

    added conditional to check if xml tag exists before accessing the tag

--- src/patchboard.cpp
+++ src/patchboard.cpp
@@ -12,60 +12,69 @@ bool Patchboard::loadFile(const string& patchboardFile)
 		patchcords.clear();
 
 		xmlSettings.pushTag("patchboard");
-		xmlSettings.pushTag("settings");
+		if (xmlSettings.tagExists("settings"))
+		{
+			xmlSettings.pushTag("settings");
+			if (xmlSettings.tagExists("input"))
+			{
+				xmlSettings.pushTag("input");
+				int count = 0;
+				cout << "<" << xmlSettings.storedHandle.ToElement()->Value() << ">" << endl;
+				for (auto child = xmlSettings.storedHandle.ToNode()->FirstChild(); child; child = child->NextSibling())
+				{
+					count++;
+					cout << indent << child->ToElement()->Value() << ": " << child->ToElement()->GetText() << endl;
+					settings.input[child->ToElement()->Value()] = child->ToElement()->GetText();
+				}
+				if (count == 0)
+				{
+					cout << "Patchboard::loadFile -- no inputs" << endl;
+					return false; // no inputs
+				}
+				xmlSettings.popTag(); // pop input
+			}
+			if (xmlSettings.tagExists("output"))
+			{
+				xmlSettings.pushTag("output");
+				int count = 0;
+				cout << "<" << xmlSettings.storedHandle.ToElement()->Value() << ">" << endl;
+				for (auto child = xmlSettings.storedHandle.ToNode()->FirstChild(); child; child = child->NextSibling())
+				{
+					count++;
+					cout << indent << child->ToElement()->Value() << ": " << child->ToElement()->GetText() << endl;
+					settings.output[child->ToElement()->Value()] = child->ToElement()->GetText();
+				}
+				if (count == 0)
+				{
+					cout << "Patchboard::loadFile -- no outputs" << endl;
+					return false; // no outputs
+				}
+				xmlSettings.popTag(); // pop output
+				xmlSettings.popTag(); // pop settings
+			}
+		}
+		if (xmlSettings.tagExists("patchcords"))
+		{
+			xmlSettings.pushTag("patchcords");
+			int nPatches = xmlSettings.getNumTags("patch");
+			for (int p = 0; p < nPatches; p++)
+			{
+				xmlSettings.pushTag("patch", p);
+				cout << "<patch>" << endl;
+				string key = xmlSettings.getValue("input", "");
+				string value = xmlSettings.getValue("output", "");
+				vector<string> values = patchcords[key];
+				values.push_back(value);
+				cout << indent << key << ":" << value << endl;
+				patchcords[key] = values;
+				xmlSettings.popTag(); // pop patch
+			}
+			xmlSettings.popTag(); // pop patchcords
+			xmlSettings.popTag(); // pop patchboard
+			if (nPatches == 0) return false; // no patches
 
-		xmlSettings.pushTag("input");
-		int count = 0;
-		cout << "<" << xmlSettings.storedHandle.ToElement()->Value() << ">" << endl;
-		for (auto child = xmlSettings.storedHandle.ToNode()->FirstChild(); child; child = child->NextSibling())
-		{
-			count++;
-			cout << indent << child->ToElement()->Value() << ": " << child->ToElement()->GetText() << endl;
-			settings.input[child->ToElement()->Value()] = child->ToElement()->GetText();
+			return true;
 		}
-		if (count == 0)
-		{
-			cout << "Patchboard::loadFile -- no inputs" << endl;
-			return false; // no inputs
-		}
-		xmlSettings.popTag(); // pop input
-
-		xmlSettings.pushTag("output");
-		count = 0;
-		cout << "<" << xmlSettings.storedHandle.ToElement()->Value() << ">" << endl;
-		for (auto child = xmlSettings.storedHandle.ToNode()->FirstChild(); child; child = child->NextSibling())
-		{
-			count++;
-			cout << indent << child->ToElement()->Value() << ": " << child->ToElement()->GetText() << endl;
-			settings.output[child->ToElement()->Value()] = child->ToElement()->GetText();
-		}
-		if (count == 0)
-		{
-			cout << "Patchboard::loadFile -- no outputs" << endl;
-			return false; // no outputs
-		}
-		xmlSettings.popTag(); // pop output
-		xmlSettings.popTag(); // pop settings
-
-		xmlSettings.pushTag("patchcords");
-		int nPatches = xmlSettings.getNumTags("patch");
-		for (int p = 0; p < nPatches; p++)
-		{
-			xmlSettings.pushTag("patch", p);
-			cout << "<patch>" << endl;
-			string key = xmlSettings.getValue("input", "");
-			string value = xmlSettings.getValue("output", "");
-			vector<string> values = patchcords[key];
-			values.push_back(value);
-			cout << indent << key << ":" << value << endl;
-			patchcords[key] = values;
-			xmlSettings.popTag(); // pop patch
-		}
-		xmlSettings.popTag(); // pop patchcords
-		xmlSettings.popTag(); // pop patchboard
-		if (nPatches == 0) return false; // no patches
-
-		return true;
 	}
 	else
 	{
